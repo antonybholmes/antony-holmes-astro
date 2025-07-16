541e90610d22baef9003f07bd33420d01059f308
Antony Holmes <antony@antonybholmes.com>
2025-07-16 00:13:42 -0400

    Added: Added animation to logo

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,14 +1,24 @@
+import { cn } from '@/lib/shadcn-utils'
+import { useState } from 'react'
 import { HCenterRow } from '../layout/h-center-row'
 
 export function Logo() {
-  //const [hover, setHover] = useState(false)
+  const [hover, setHover] = useState<null | boolean>(null)
   //const [down, setDown] = useState(false)
 
   return (
-    <HCenterRow className="items-center w-12 h-12">
+    <HCenterRow
+      className="items-center w-12 h-12"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <a
         href="/"
-        className="flex w-10 h-10 aspect-square rounded-[1rem] animate-scale-then-round hover:animate-round-then-scale bg-gradient-to-br from-cyan-400 to-blue-500 flex-row items-center justify-center font-semibold text-lg text-white trans-all"
+        className={cn(
+          'flex w-10 h-10 aspect-square rounded-[1rem] bg-gradient-to-br from-cyan-400 to-blue-500 flex-row items-center justify-center font-semibold text-lg text-white trans-all',
+          hover === true && 'animate-round-then-scale',
+          hover === false && 'animate-scale-then-round'
+        )}
         aria-label="Home"
       >
         ah
