fb103a094aff9880d2695c389fe86fb1e80d2c3b
Antony Holmes <antony@antonyholmes.dev>
2026-01-17 00:17:52 -0500

    fix: Bug fixes and updates.

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -2,6 +2,11 @@ import { gsap } from 'gsap'
 import { DraftingCompass } from 'lucide-react'
 import { useEffect, useRef } from 'react'
 
+const LOGO_CLS = `flex w-10 h-10 aspect-square rounded-2xl bg-blue-500 
+  hover:bg-violet-400 flex-row items-center justify-center font-bold 
+  text-lg text-white hover:rounded-none transition-all duration-500 ease-in-out
+  relative group overflow-hidden`
+
 export function Logo() {
   const ref = useRef<HTMLAnchorElement>(null)
   const animationRef = useRef<gsap.core.Timeline>(null)
@@ -30,13 +35,11 @@ export function Logo() {
   }
 
   return (
-    <a
-      ref={ref}
-      href="/"
-      className="flex w-10 h-10 aspect-square rounded-2xl bg-blue-500 hover:bg-violet-400 flex-row items-center justify-center font-bold text-lg text-white hover:rounded-none transition-all duration-500 ease-in-out"
-      aria-label="Home"
-    >
-      <DraftingCompass />
+    <a ref={ref} href="/" className={LOGO_CLS} aria-label="Home">
+      <DraftingCompass className="absolute left-1/2 top-1/2 -translate-1/2 group-hover:opacity-0 group-hover:scale-80 transition-all duration-500 ease-in-out" />
+      <span className="absolute left-1/2 top-1/2 -translate-1/2 opacity-0 group-hover:opacity-100 group-hover:scale-100 scale-80 transition-all duration-500 ease-in-out">
+        ah
+      </span>
     </a>
   )
 }
