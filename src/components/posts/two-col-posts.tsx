85dd9af891bb7d1c955f20e85ed1d8844385698f
Antony Holmes <antony@antonyholmes.dev>
2026-01-29 22:29:26 -0500

    fix: Bug fixes and updates.

---
+++ src/components/posts/two-col-posts.tsx
@@ -0,0 +1,36 @@
+import { cn } from '@/lib/shadcn-utils'
+import type { IPostsProps } from './hero-posts'
+import { RestPost } from './rest-post'
+
+export function TwoColPosts({
+  posts,
+  showSectionLinks = true,
+  className,
+}: IPostsProps) {
+  return (
+    <>
+      <ul className={cn('grid md:hidden gap-12 grid-cols-1', className)}>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <RestPost
+              post={post}
+              className={cn(index > 0 && 'pt-6 border-t border-border/50')}
+              showSectionLinks={showSectionLinks}
+            />
+          </li>
+        ))}
+      </ul>
+      <ul className={cn('hidden md:grid gap-12 grid-cols-2', className)}>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <RestPost
+              post={post}
+              className={cn(index > 1 && 'pt-6 border-t border-border/50')}
+              showSectionLinks={showSectionLinks}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
